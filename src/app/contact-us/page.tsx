debece9b01bf1186cdaf6c8b12648e497596fabc
Muhammad Ilyas <muhammad.ilyas@jubelio.com>
2023-06-25 12:40:14 +0700

    feat: add contacu us

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <main className="max-w-5xl mx-auto px-2 md:px-4">
+      <h2>CONTACT US</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
+        culpa eius! Optio necessitatibus facilis exercitationem vero cumque sed
+        explicabo in.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
+        culpa eius! Optio necessitatibus facilis exercitationem vero cumque sed
+        explicabo in.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
+        culpa eius! Optio necessitatibus facilis exercitationem vero cumque sed
+        explicabo in.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
+        culpa eius! Optio necessitatibus facilis exercitationem vero cumque sed
+        explicabo in.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
+        culpa eius! Optio necessitatibus facilis exercitationem vero cumque sed
+        explicabo in.
+      </p>
+    </main>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
